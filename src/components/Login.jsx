2ad239ec9fbecc7e752367b8cc7b580984366d9c
Jeremy <jeremy.gitau@student.moringaschool.com>
2024-11-21 08:55:45 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setError(null);
 
     try {
-      const response = await fetch("/api/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
